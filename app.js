c733c0b8caa07ee3e8d6d482c02b02ca4d5f43b6
Jordan Wilkerson <jordangwilkerson@gmail.com>
2021-02-21 18:38:40 -0700

    here we go

---
+++ app.js
@@ -0,0 +1,264 @@
+class Aardvark {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    }
+    
+}
+
+class Baboon {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    }
+
+}
+class Crocodile {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Draugr {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Echidna {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Fox {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class GreatWhiteShark {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class HornyToad {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Ibex {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Jaberwocky {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Kangaroo {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Llama {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Moose {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Numbat {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Opposum {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Possum {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Quetzalcoatl {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Rat {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class StabbyFish {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Tapir {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class UlaAiAawane {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Vole {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Wombat {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Xerus {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Yabby {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+class Zebu {
+    constructor (name,type,height, weight)
+    {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.weight = weight
+    } 
+}
+
+let Aardvark1 = new Aardvark("Phil", "Rolly","Short", "Kinda heavy")
+let Baboon1 = new Baboon("Frank", "Monke", "Tallish", "Light")
+let Crocodile1= new Crocodile("Harry","Bitey","Longer than me", "Chonky")
+let Draugr1= new Draugr("Doug", "Evil", "Bought my height", "bought my weight, plus War Axe")
+let Echidna1 = new Echidna("Alice", "Rolly", "Real Short","Real Light")
+let Fox1 = new Fox("Howard", "Tricky", "Small", "Decently Light")
+let GreatWhiteShark1 = new GreatWhiteShark("Terrence", "Bitey", "Real Big","A lot, we think")
+let HornyToad1 = new HornyToad("Hugh", "Horned", "Toad", "Toad")
+let Ibex1 = new Ibex("Laura", "Horned", "Big Horns", "Mostly Horns")
+let Jaberwocky1 = new Jaberwocky("?????","?????","?????","?????")
+let Kangaroo1 = new Kangaroo("Rocky", "Punchy", "Bought my size", "About my weight class")
+let Llama1 = new Llama("CAAAARRRRLLLLL","Sociopath","decently tall", "lots of emotional baggage")
+let Moose1 = new Moose("Mark", "Moose","Huge","Real Huge")
+let Numbat1 = new Numbat("Tasha", "Not Sure", "Small, we think", "We only have a picture")
+let Opposum1 = new Opposum("Trash", "Trash", "Trash", "Screams at Trash")
+let Possum1 = new Possum("Less Trash", "Not quite as Trashy", "Short", "Quite Yeetable")
+let Quetzalcoatl1 = new Quetzalcoatl("Rob", "MesoAmerican ender of the world", "About the size of the globe","Not fully sure")
+let Rat1 = new Rat("Frank", "A dapper gentleman", "smoll", "big at heart")
+let StabbyFish1 = new StabbyFish("Yorvo","Stabby", "Long", "Heavier than you think")
+let Tapir1 = new Tapir("Patricia", "Chill", "Not that heavy", "Dog-Size")
+let UlaAiAawane1 = new UlaAiAawane("Scarlet", "Birb", "Not Large", "Not Heavy")
+let Vole1 = new Vole("Pat", "Has a cat", "kinda a brat", "likes bats")
+let Wombat1 = new Wombat("Wooby","Vooby", "Scooby", "Dooby")
+let Xerus1 = new Xerus("Bob", "Small African Ground Squirrel", "Small", "Kickable")
+let Yabby1 = new Yabby("Ronald", "Lobster-ish", "Lobster-size", "About as heavy as a lobster")
+let Zebu1 = new Zebu("Ron", "Yakish", "Tall", "Heavy")
